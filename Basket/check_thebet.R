0a0d1acdac0032bc34c1019d5b66a8f340f0f406
Josh Goldstein <josh.goldstein@berkeley.edu>
2016-12-09 20:45:12 -0800

    included source() for check_thebet.R

---
+++ Basket/check_thebet.R
@@ -0,0 +1,51 @@
+{{
+.qnumber1.1 = 1.1
+.correct.answer1.1 = "D"
+.hint1.1 = "Prices are indexed to 1900 and are adjusted for inflation"
+.instructions1.1 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation1.1 = "Real prices adjust for inflation"
+}}
+{{
+.qnumber1.2 = 1.2
+.correct.answer1.2 = "B"
+.hint1.2 = "https://en.wikipedia.org/wiki/Chromium"
+.instructions1.2 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation1.2 = "According to wikipedia, 85 percent is apparently used
+.for this one purpose. Are there substitutes for protecting steel?"
+}}
+{{
+.qnumber2.1 = 2.1
+.correct.answer2.1 = "C"
+.hint2.1 = "Sorry, no hint"
+.instructions2.1 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation2.1 = "There's clearly a lot of volitility. Although the price in 2015 is higher than in 1900, this doesn't look like a clear trend, since for much of the 20th century the price was lower than in 1900."
+}}
+{{
+.qnumber2.2 = 2.2
+.correct.answer2.2 = "B"
+.hint2.2 = "All are reasonable, except C which is incorrect. Try again if you didn't answer B."
+.instructions2.2 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation2.2 = "Apparently, tin prices were controlled in part by a cartel of producers. See https://www.itri.co.uk/index.php?option=com_mtree&task=att_download&link_id=49605&cf_id=24 for a discussion from an industry research source."
+}}
+{{
+.qnumber3.1 = 3.1
+.correct.answer3.1 = "B"
+.hint3.1 = "Prices went up"
+.instructions3.1 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation3.1 = "An increase in prices suggests an increase in
+.'scarcity' (according to Simon)."
+}}
+{{
+.qnumber3.2 = 3.2
+.correct.answer3.2 = "A"
+.hint3.2 = "Insert a line: 'print(original.bet.result)' in the code chunk to see the result of the bet."
+.instructions3.2 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation3.2 = "A negative number means prices went down, and so Simon would have won."
+}}
+{{
+.qnumber2.4 = 2.4
+.correct.answer2.4 = "B"
+.hint2.4 = "Insert there are 105 bets, not 100."
+.instructions2.4 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation2.4 = "62/1052.4 = 0.58."
+}}
