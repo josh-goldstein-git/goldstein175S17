75ae63a5077c2c381ab89a0df15af4eb65f8182c
Josh Goldstein <josh.goldstein@berkeley.edu>
2017-02-01 11:09:20 -0800

    solow lab added to Inventory and Basket

---
+++ Basket/check_solow.R
@@ -0,0 +1,40 @@
+{{
+.qnumber1.1 = 1.1
+.correct.answer1.1 = "C"
+.hint1.1 = "The marginal product on capital is the additional output from
+.an increment of capital, the slope of the production function with
+.respect to k."
+.instructions1.1 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation1.1 = "The marginal product on capital is equal to the slope of
+.the production function, which our plot shows is declining over
+.time."
+}}
+{{
+.qnumber1.2 = 1.2
+.correct.answer1.2 = "B"
+.hint1.2 = "See our graph."
+.instructions1.2 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation1.2 = "With Cobb-Douglas production the rate of increase in
+.capital is exactly balanced with the decline in the rate of return,
+.keeping the capital share of income constant."
+}}
+{{
+.qnumber2.1 = 2.1
+.correct.answer2.1 = "A"
+.hint2.1 = "Consult your graph to see if the marginal product is declining."
+.instructions2.1 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation2.1 = "Yes, it is still declining. If you compare with
+.Cobb-Douglas, the rate of return looks like it declines more slowly,
+.especially for large values of k."
+}}
+{{
+.qnumber2.2 = 2.2
+.correct.answer2.2 = "A"
+.hint2.2 = "See our graph."
+.instructions2.2 = "Replace the NA with your answer (e.g., 'A' in quotes)"
+.explanation2.2 = "We now see that the more capital we have per worker,
+.the greater the capital share of the economy is! This is what Piketty
+.argues is likely to happen. As growth rates fall, capital per worker
+.will increase, and with it an increase in the capital share of the
+.economic output."
+}}
